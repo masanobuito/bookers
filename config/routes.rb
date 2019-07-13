593603c5b7e93017be63c17b5526535a59b83304
Apple <apple@ApplenoMacBook-Pro.local>
2019-07-13 18:29:21 +0900

    [7/13-2] 4-8

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books
+  root to:'books#top'
+  # get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  post 'books' => 'books#create'
+end
