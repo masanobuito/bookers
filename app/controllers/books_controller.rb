41a7f65e75daf7139ea1c089541b4f68194c798b
Apple <apple@ApplenoMacBook-Pro.local>
2019-07-14 13:13:52 +0900

    [7/14] 4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   def show
-    # @book = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def top
@@ -22,7 +22,19 @@ class BooksController < ApplicationController
   end
 
   def edit
-    # @book = Book.find(params[:id])
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+      book.update(book_params)
+      redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+      book.destroy
+      redirect_to books_path
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root to:'books#top'
   # get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   post 'books' => 'books#create'
+
 end
